12ba8c4d38295f5f4ca9099a57a7f1e38ebbc376
MomoPako <momopako@Pakos-Book.local>
2023-11-28 13:40:38 +0100

    Initial commit

---
+++ composants/footer.php
@@ -0,0 +1,22 @@
+<footer class="responsive_footer" style="margin-top: 30px;">
+    <div style="display: flex; justify-content: space-evenly;">
+        <div>
+            <p style="text-align: center;">Contact par mail :
+            <ul>
+                <li><a href="mailto:diabate.mohamed2609@gmail.com">diabate.mohamed2609@gmail.com</a></li>
+                <li><a href="mailto:bilal.mohammad2709@gmail.com">bilal.mohammad2709@gmail.com</a></li>
+                <li><a href="mailto:barchat.mohamed.sio@gmail.com">barchat.mohamed.sio@gmail.com</a></li>
+            </ul>
+            </p>
+        </div>
+        <div>
+            <p class="text-center">Contact par téléphone :
+            <ul>
+                <li><a href="tel:+33783529587">Mohamed DIEBAKATE</a></li>
+                <li><a href="tel:+33652885187">Bilal MOHAMMAD</a></li>
+                <li><a href="tel:+33783475273">Mohamed BARCHAT</a></li>
+            </ul>
+            </p>
+        </div>
+    </div>
+</footer>
---
+++ controleur/config_bdd.php
@@ -0,0 +1,11 @@
+<?php
+    // Connex BDD Mac
+    $serveur = "localhost";
+    $bdd = "jo_paris";
+    $user = "Pako";
+    $mdp = "26092001MPk";
+
+    //Connex BDD PC
+    $serveur2 = "localhost";
+    $mdp2 = "";
+?>
